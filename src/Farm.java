2de1a2201613ace75b6708cc93ac1676735dd665
svec3 <adam@tsv.cz>
2024-03-08 12:51:39 +0100

    added planting

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,24 @@
+import java.util.concurrent.Flow;
+
 public class Farm {
+
+    private Flower[] flowers;
+    private int farmSize;
+
+    public Farm(int flowersSize, int farmSize) {
+        this.flowers = new Flower[flowersSize];
+        this.farmSize = farmSize;
+    }
+
+    public void plant(Flower other){
+        for (int i = 0; i < flowers.length; i++){
+            if (flowers[i] == null){
+                flowers[i] = other;
+                return;
+            }
+        }
+        throw new RuntimeException(flowers.length + "/" + flowers.length + " flowers!");
+    }
 
 
 
