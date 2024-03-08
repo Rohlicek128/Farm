b15806206745b09986b687f30ae78b076ad39e82
svec3 <adam@tsv.cz>
2024-03-08 13:31:40 +0100

    added methods: removeFlower, flowerToIndex, harvest, waterFlower

--- src/Farm.java
+++ src/Farm.java
@@ -1,25 +1,57 @@
-import java.util.concurrent.Flow;
-
 public class Farm {
 
     private Flower[] flowers;
-    private int farmSize;
+    private double money;
+    private final double land;
+    private double landLeft;
 
-    public Farm(int flowersSize, int farmSize) {
+    public Farm(int flowersSize, double land, int startingMoney) {
         this.flowers = new Flower[flowersSize];
-        this.farmSize = farmSize;
+        this.land = land;
+        this.landLeft = land;
+        this.money = startingMoney;
     }
 
     public void plant(Flower other){
+        if (landLeft < other.neededArea) throw new RuntimeException("Not enough space!");
         for (int i = 0; i < flowers.length; i++){
             if (flowers[i] == null){
                 flowers[i] = other;
+                landLeft -= flowers[i].neededArea;
                 return;
             }
         }
         throw new RuntimeException(flowers.length + "/" + flowers.length + " flowers!");
     }
 
+    public void removeFlower(Flower f){
+        for (int i = 0; i < flowers.length; i++){
+            if (f == flowers[i]){
+                flowers[i] = null;
+                return;
+            }
+        }
+        throw new RuntimeException("Not found!");
+    }
 
+    public int flowerToIndex(Flower f){
+        for (int i = 0; i < flowers.length; i++){
+            if (f == flowers[i]) return i;
+        }
+        throw new RuntimeException("Not found");
+    }
+
+    public void harvest(){
+        for (Flower f : flowers){
+            if (f.isGrown){
+                money += f.getPrice();
+                removeFlower(f);
+            }
+        }
+    }
+
+    public void waterFlower(Flower f){
+        flowers[flowerToIndex(f)].water();
+    }
 
 }
--- src/Flower.java
+++ src/Flower.java
@@ -4,13 +4,57 @@ public abstract class Flower {
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
-    protected boolean isGrown;
+    protected boolean isGrown = false;
 
     public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
         this.name = name;
         this.price = price;
         this.neededArea = neededArea;
         this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getNeededArea() {
+        return neededArea;
+    }
+
+    public void setNeededArea(double neededArea) {
+        this.neededArea = neededArea;
+    }
+
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public boolean isGrown() {
+        return isGrown;
+    }
+
+    public void setGrown(boolean grown) {
+        isGrown = grown;
+    }
+
+    public void water(){
+
     }
 
     @Override
